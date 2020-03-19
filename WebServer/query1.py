e81cf9528ad356fa96cab5a5265786cdd8c6c10f
Runqi Tian <runqitian@Runqis-MacBook-Pro.local>
2020-03-18 21:11:46 -0400

    webserver for query1

---
+++ WebServer/query1.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import jsonify
+import pandas as pd
+import json
+app = Flask(__name__)
+
+df_raw = pd.read_csv("./dataset/in1.csv", header=None)
+
+current_idx = 0
+chunk_size=1000
+finish = False
+
+def get_chunk():
+    global finish
+    global current_idx
+    global chunk_size
+    if finish:
+        return None
+    if current_idx + chunk_size>len(df_raw):
+        df_chunk = df_raw.iloc[current_idx:len(df_raw),:]
+        finish=True
+    else:
+        df_chunk = df_raw.iloc[current_idx:current_idx+chunk_size,:]
+        current_idx = current_idx+chunk_size
+    return df_chunk
+
+def transfer_dict(df_chunk):
+    result = []
+    for _,each in df_chunk.iterrows():
+        temp = {}
+        temp['i'] = int(each[0])
+        temp['voltage'] = each[1]
+        temp['current'] = each[2]
+        result.append(temp)
+    return result
+
+
+
+@app.route('/data/1', methods=["GET"])
+def hello_world():
+    df_chunk = get_chunk()
+    if df_chunk is None:
+        return jsonify({"records": []})
+    else:
+        return jsonify({"records": transfer_dict(df_chunk)})
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80)
