faa8828bf0bce066c75ca9e1f40b188c4b5818d9
Jin Qin <qin_jin@live.com>
2020-03-27 01:17:40 -0700

    update: finish computeAndEvaluateLoss

--- src/main/java/EventDector.java
+++ src/main/java/EventDector.java
@@ -1,32 +1,18 @@
 import entities.DetectedEvent;
-import entities.Event;
-import entities.Feature;
 import entities.KeyedFeature;
-import entities.KeyedFeature;
-import entities.RawData;
 import entities.Window2;
-import scala.collection.parallel.ParIterableLike.FlatMap;
 
 import org.apache.commons.math3.ml.clustering.Cluster;
 import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction;
-import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.util.Collector;
-import utils.Metrics;
 import java.util.*;
 
 public class EventDector {
 
     private double dbscanEps;
     private int dbscanMinPoints;
+    private int lossThresh = 40;
 
     private Map<Integer, ClusterStructure> clusteringStructure;
 
@@ -99,10 +85,55 @@ public class EventDector {
      */
     private Tuple3<Integer, Integer, List<Integer>>
     computeAndEvaluateLoss(List<Tuple3<Integer, Integer, List<Integer>>> checkedClusters) {
+        List<Integer> eventModelLossList = new ArrayList<>();
+        int minLossIndex = -1;
+        int minLoss = Integer.MAX_VALUE;
+        for (int i = 0; i < checkedClusters.size(); i++) {
+            Tuple3<Integer, Integer, List<Integer>> item = checkedClusters.get(i);
+            Integer c1 = item.f0;
+            Integer c2 = item.f1;
+            List<Integer> eventIntervalIndices = item.f2;
+            Integer lowerEventBound_U = eventIntervalIndices.get(0) - 1;    // the interval starts at u + 1
+            Integer upperEventBound_V = eventIntervalIndices.get(eventIntervalIndices.size() - 1) + 1; // the interval ends at v -1
+            List<Integer> c1Indices = this.clusteringStructure.get(c1).idxList;
+            List<Integer> c2Indices = this.clusteringStructure.get(c2).idxList;
+            
+            Tuple3<Integer, Integer, Integer> errorsNums = computeErrorNumbers(c1Indices, c2Indices, lowerEventBound_U, upperEventBound_V);
+            Integer eventModelLoss = errorsNums.f0 + errorsNums.f1 + errorsNums.f2; // a + b + c
+            eventModelLossList.add(eventModelLoss);
+
+            if (eventModelLoss < minLoss) {
+                minLoss = eventModelLoss;
+                minLossIndex = i;
+            }
+        }
+
+        if (minLoss <= this.lossThresh)
+            return checkedClusters.get(minLossIndex);
+
         return null;
     }
 
     private void rolebackBackwardPass() {
         // TO DO
+    }
+
+    private Tuple3<Integer, Integer, Integer> computeErrorNumbers(List<Integer> c1Indices, List<Integer> c2Indices, 
+                                                                  Integer lowerThresh, Integer upperThresh) {
+        Tuple3<Integer, Integer, Integer> counter = new Tuple3<>();
+        
+        // c2 <= u || u < c2 < v, c2 should >= v
+        for (int i = 0; i < c2Indices.size(); i++) {
+            if (c2Indices.get(i) <= lowerThresh) counter.f0++;
+            else if (c2Indices.get(i) < upperThresh) counter.f2++;
+        }
+
+        // c1 >= v || u < c1 < v, c1 should <= u
+        for (int i = 0; i < c1Indices.size(); i++) {
+            if (c1Indices.get(i) >= upperThresh) counter.f1++;
+            else if (c1Indices.get(i) > lowerThresh) counter.f2++;
+        }
+
+        return counter;
     }
 }
