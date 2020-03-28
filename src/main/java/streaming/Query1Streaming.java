38b753762fd0e6600c4c455b0b88926fd36c5df3
Jin Qin <qin_jin@live.com>
2020-03-27 19:00:33 -0700

    fix: merge conflict

--- src/main/java/streaming/Query1Streaming.java
+++ src/main/java/streaming/Query1Streaming.java
@@ -1,10 +1,12 @@
 package streaming;
 
+import entities.DetectedEvent;
 import entities.Feature;
 import entities.KeyedFeature;
 import entities.PredictedEvent;
 import entities.Window2;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.util.Collector;
 import utils.Config;
 
@@ -31,10 +33,22 @@ public class Query1Streaming {
         private static Window2 w2 = new Window2();
         private static EventDector ed = new EventDector();
 
+        private static int windowStartIndex = 0;
+
         @Override
         public void flatMap(KeyedFeature value, Collector<PredictedEvent> out) throws Exception {
             w2.addElement(value);
-            ed.predict(w2);
+            PredictedEvent e = ed.predict(w2);
+            if (e != null) out.collect(e);
+        }
+    }
+
+    public static class DetectedEventMapper implements MapFunction<PredictedEvent, DetectedEvent> {
+        private static final long serialVersionUID = 8598912969202017900L;
+
+        @Override
+        public DetectedEvent map(PredictedEvent value) throws Exception {
+            return null;
         }
     }
 }
